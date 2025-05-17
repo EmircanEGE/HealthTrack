52a437a0989aa2345cdcb616dc0a4250a38f81ae
Emircan EGE <ege61emir@gmail.com>
2025-05-17 19:10:10 +0300

    feat(infrastructure): Add AppDbContext and entity DbSets

---
+++ HealthTrack.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HealthTrack.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthTrack.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Exercise> Exercises => Set<Exercise>();
+        public DbSet<Nutrition> Nutritions => Set<Nutrition>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
