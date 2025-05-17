dd275c565f6d801443b6c3422fd6ae52d3c13a5f
Emircan EGE <ege61emir@gmail.com>
2025-05-17 19:25:17 +0300

    feat(infrastructure): Implement generic Repository

---
+++ HealthTrack.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using HealthTrack.Core.Entities;
+using HealthTrack.Core.Exceptions;
+using HealthTrack.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthTrack.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            return entity ?? throw new NotFoundException(typeof(T).Name, id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T Entity)
+        {
+            await _context.Set<T>().AddAsync(Entity);
+            await _context.SaveChangesAsync();
+            return Entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
