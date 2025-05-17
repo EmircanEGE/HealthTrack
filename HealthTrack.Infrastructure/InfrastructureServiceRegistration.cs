8ba8f6c74c2cf929ae91237d38457f034aa0ef6a
Emircan EGE <ege61emir@gmail.com>
2025-05-17 19:52:01 +0300

    feat(infrastructure): Configure DI and connection string

---
+++ HealthTrack.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using HealthTrack.Infrastructure.Data;
+using HealthTrack.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthTrack.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            return services;
+        }
+    }
+}
